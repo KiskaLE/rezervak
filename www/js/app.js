149a99dfd9502b5074d9ca59c5f767a4339f984e
Vojtech Kylar <vojtakylar@seznam.cz>
2023-11-03 13:45:14 +0100

    update

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,11 @@
 import naja from 'naja';
 import netteForms from 'nette-forms';
+import Chart from 'chart.js/auto';
+
 
 window.Nette = netteForms;
 window.Naja = naja;
+window.Chart = Chart
 
 document.addEventListener('DOMContentLoaded', naja.initialize.bind(naja));
 
