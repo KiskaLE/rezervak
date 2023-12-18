2215826d53057edf5d5c56b62f723df4740d4368
Vojtech Kylar <vojtakylar@seznam.cz>
2023-12-18 15:15:01 +0100

    full calendar update

--- www/assets/admin/js/fullCalendar.js
+++ www/assets/admin/js/fullCalendar.js
@@ -1,13 +1,9 @@
-
-
 try {
     let calendar
     
 } catch (error) {
     
 }
-
-
 /**
  * Create a full calendar with the specified slot duration, calendar period, service duration, service name, and parent ID.
  *
@@ -22,14 +18,21 @@ function createFullCalendar(slotDuration ,calendarPeriod, serviceDuration, servi
     calendar = new FullCalendar.Calendar(document.getElementById(parentId), {
             locale: 'cs',
             eventOverlap: false,
-            aspectRatio: 2,
+            aspectRatio: 1,
             selectable: true,
             editable: false,
-            initialView: 'timeGridFourDay',
+            themeSystem: "standard",
+            initialView: 'dayGridMonth',
+            showNonCurrentDates: false,
+            headerToolbar: {
+                left: "title",
+                center: "dayGridMonth",
+                right: "today prev,next"
+            },
             firstDay: 1,
             timeZone: 'UTC',
             views: {
-                timeGridFourDay: {
+                timeGridSevenDay: {
                     type: 'timeGrid',
                     duration: {
                         days: 7},
@@ -40,66 +43,78 @@ function createFullCalendar(slotDuration ,calendarPeriod, serviceDuration, servi
                 month:    'Měsíc',
                 week:     'Týden',
                 day:      'Den',
-                list:     'List'
+                list:     'List',
+                timeGridSevenDay: "Den"
             },
             titleFormat: { year: 'numeric', month: 'long', day: 'numeric' },
             slotDuration: slotDuration,
             nowIndicator: true,
             select: function (selectionInfo) {
-                // Clear any previous selections
-                calendar.unselect(); // Set your event duration here
+                const currentView = calendar.view;
+                if (currentView.type === 'timeGridDay') {
+                        // Clear any previous selections
+                    calendar.unselect(); // Set your event duration here
 
-                const numberOfPeriods = Math.ceil(serviceDuration/calendarPeriod);
+                    const numberOfPeriods = Math.ceil(serviceDuration/calendarPeriod);
 
-                // Start time of the first event
-                let startTime = window.Moment(selectionInfo.start);
-                let endTime = window.Moment(selectionInfo.end);
+                    // Start time of the first event
+                    let startTime = window.Moment(selectionInfo.start);
+                    let endTime = window.Moment(selectionInfo.end);
 
-                // Loop to create back-to-back events
-                let count = 0;
-                while (startTime < endTime) {
-                    let eventEndTime = startTime.clone().add(serviceDuration, 'minutes');
+                    // Loop to create back-to-back events
+                    let count = 0;
+                    while (startTime < endTime) {
+                        let eventEndTime = startTime.clone().add(serviceDuration, 'minutes');
 
-                    // If the calculated end time exceeds the selected end time, break the loop
-                    if (eventEndTime > endTime) {
-                        if (count !== 0) {
-                            break;
+                        // If the calculated end time exceeds the selected end time, break the loop
+                        if (eventEndTime > endTime) {
+                            if (count !== 0) {
+                                break;
+                            }
                         }
-                    }
 
-                    // Function to check for event conflicts
-                    function isEventConflict(eventStart, eventEnd) {
-                        let events = calendar.getEvents();
-                        return events.some(function (existingEvent) {
-                            return (
-                                eventStart < existingEvent.end && eventEnd > existingEvent.start
-                            );
-                        });
-                    }
+                        // Function to check for event conflicts
+                        function isEventConflict(eventStart, eventEnd) {
+                            let events = calendar.getEvents();
+                            return events.some(function (existingEvent) {
+                                return (
+                                    eventStart < existingEvent.end && eventEnd > existingEvent.start
+                                );
+                            });
+                        }
 
-                    // Check for conflicts
-                    if (!isEventConflict(startTime, eventEndTime)) {
-                        // If no conflict, add the event
-                        calendar.addEvent({
-                            title: serviceName,
-                            start: startTime.toDate(),
-                            end: eventEndTime.toDate()
-                        });
-                    }
+                        // Check for conflicts
+                        if (!isEventConflict(startTime, eventEndTime)) {
+                            // If no conflict, add the event
+                            calendar.addEvent({
+                                title: serviceName,
+                                start: startTime.toDate(),
+                                end: eventEndTime.toDate()
+                            });
+                        }
 
-                    // Set start time for the next event to be the end time of the current event
-                    startTime.add(numberOfPeriods * calendarPeriod, 'minutes');
-                    count++
+                        // Set start time for the next event to be the end time of the current event
+                        startTime.add(numberOfPeriods * calendarPeriod, 'minutes');
+                        count++
+                    }
+                    // Clear the selection
+                    calendar.unselect();
                 }
-
-                // Clear the selection
-                calendar.unselect();
+                
             },
             eventClick: function (info) {
-                info.event.remove();
+                const currentView = calendar.view;
+                if (currentView.type === 'timeGridDay') {
+                    info.event.remove();
+                }
+            },
+            dateClick: function (info) {
+                const currentView = calendar.view;
+                if (currentView.type === 'dayGridMonth') {
+                    calendar.changeView('timeGridDay', info.dateStr); 
+                }
             }
         });
-        calendar.destroy();
         calendar.render();
         
     
